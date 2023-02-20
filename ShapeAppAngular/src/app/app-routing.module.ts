8e7224892f762dfd1b02491e1242a6b33c61a784
Adrien Houen <adhouen@yahoo.com>
2023-02-21 00:46:10 +0100

    BMI

--- ShapeAppAngular/src/app/app-routing.module.ts
+++ ShapeAppAngular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { CalculBmiComponent } from './components/user/calcul-bmi/calcul-bmi.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+     // BMI
+     {
+      path: 'bmi',
+      component:CalculBmiComponent
+
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
